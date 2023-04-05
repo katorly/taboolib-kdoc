f87d15febcfe31400222c8b84a212a67143d31f9
Katorly <1649643021@qq.com>
2023-04-05 13:51:31 +0000

    辅助说明

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# taboolib-kdoc
-Taboolib-6 Kotlin Documentation | Taboolib-6 Kotlin 文档（非官方，使用 Dokka 构建）
+# 常见疑问 & 构建指南
+> **Warning**
+> 本 README 写于 2023-04-05，有些内容可能不准确或已经过期。
+
+## 可能的疑问
+### 为什么要提供文档？直接在IDEA里面F12不行么？
+Taboolib 的作者自从 6.0 版本以来，就没有再在[原本用于提供 Javadoc 的地方](https://jd.tabooproject.org)提供开发文档。  
+可以，但我习惯翻阅网页版文档。  
+### 什么是Dokka？
+[Dokka](https://github.com/Kotlin/dokka) 是 Kotlin 文档生成工具，使用它，我们可以为 Kotlin 项目（Java 也可以）方便快捷地生成类似于 Javadoc 和其它格式的文档。  
+### 为什么不把全部子项目都放入文档的侧栏导航中，而是另写一个索引页面？
+在 Taboolib 项目中使用 Dokka 进行 `dokkaHtmlMultiModule` 任务构建 Kotlin 文档时，无法生成最后汇总的那个 Kotlin 文档页面（报错，显示没有仓库），因此只能额外写一个索引页面。  
+这也是下面 构建指南 中不使用 `dokkaHtmlMultiModule` 而选择在每个子项目中执行 `dokkaHtml` 的原因。  
+### 文档不是最新版本，为什么不更新/如何更新？
+Taboolib 的小版本变动不大，而由于项目过大，生成一次全套的 Kotlin 文档需要耗费大量时间（依赖库就有几个G）。因此，当 Taboolib 出现大版本更新或大幅度变动时，本仓库会对相关 Kotlin 文档进行更新。如果您需要最新版本，可以参照下面的 构建指南 自行构建（并欢迎您提交 Pull Request 为所有人更新文档）。  
+### 文档侧栏一片空白？
+正常情况下，本文档使用 Github Pages 托管，每个子项目文档不会出现没有侧栏导航的问题。如果出现这种问题，那么很遗憾，您只能自行下载文档到本地并用 Intellij IDEA 作为网页服务打开（不能使用静态网页打开），这是 Dokka 的“特性”。  
+### 文档没看明白，xxx是什么意思/怎么用？
+本仓库仅提供 Taboolib 的 Kotlin 文档服务，但您可以点击文档索引页面右上角的导航进入到 Taboolib 的官方文档中学习。若仍有疑问，请[联系原作者](https://github.com/TabooLib)。
+
+## 构建 Dokka 文档
+### 第1步
+克隆 [Taboolib](https://github.com/TabooLib/taboolib) 的主分支至本地，并使用 IDE 打开。推荐使用 [Intellij IDEA](https://www.jetbrains.com/idea)。  
+建议不要将 Gradle 定位在系统盘，因为 Taboolib 的依赖库有几个G。  
+### 第2步
+在项目根目录里的 `build.gradle.kts` 文件中添加以下内容：
+```kotlin
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+```
+在 `plugins` 中：
+```kotlin
+id("org.jetbrains.dokka") version "1.8.10"
+```
+在 `subprojects` 中：`
+```kotlin
+apply(plugin = "org.jetbrains.dokka")
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-base:1.8.10")
+    }
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+        separateInheritedMembers = false
+        mergeImplicitExpectActualDeclarations = false
+    }
+}
+```
+如果这一步没看懂，这里有一份 [`build.gradle.kts`](/build.gradle.kts) 供参考。  
+### 第3步
+在每个有效子项目下执行 `dokkaHtml`，不要用`dokkaHtmlMultiModule`。  
+
+例如：在 `module子项目` 的 `module-effect子项目` 中执行：`./gradlew :module:module-effect dokkaHtml`。  
+
+### 第4步
+将每个项目的 `/build/dokka/html` 目录下的文件集中，并依据 `module`, `platform`, `expansion` 进行分类。  
+
---
+++ build.gradle.kts
@@ -0,0 +1,116 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+
+plugins {
+    `maven-publish`
+    java
+    id("org.jetbrains.kotlin.jvm") version "1.5.10" apply false
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    id("org.jetbrains.dokka") version "1.8.10"
+}
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "com.github.johnrengelman.shadow")
+    apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "maven-publish")
+
+    repositories {
+        maven("https://libraries.minecraft.net")
+        maven("https://repo1.maven.org/maven2")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://repo.codemc.io/repository/nms/")
+        maven("http://ptms.ink:8081/repository/releases") {
+            isAllowInsecureProtocol = true
+        }
+        mavenCentral()
+    }
+
+    dependencies {
+        compileOnly(kotlin("stdlib"))
+    }
+
+    java {
+          withJavadocJar()
+         withSourcesJar()
+    }
+
+    tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+        archiveClassifier.set("")
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-XDenableSunApiLintControl"))
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf("-Xjvm-default=all")
+        }
+    }
+
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+subprojects
+    .filter { it.name != "module" && it.name != "platform" && it.name != "expansion" }
+    .filter { it.name != "module-database-core" && it.name != "module-ui-legacy" }
+    .forEach { proj ->
+        proj.publishing { applyToSub(proj) }
+    }
+
+fun PublishingExtension.applyToSub(subProject: Project) {
+    repositories {
+        maven("http://ptms.ink:8081/repository/releases") {
+            isAllowInsecureProtocol = true
+            credentials {
+                username = project.findProperty("taboolibUsername").toString()
+                password = project.findProperty("taboolibPassword").toString()
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+        mavenLocal()
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = subProject.name
+            groupId = "io.izzel.taboolib"
+            version = (if (project.hasProperty("build")) {
+                var build = project.findProperty("build").toString()
+                if (build.startsWith("task ")) {
+                    build = "local"
+                }
+                "${project.version}-$build"
+            } else {
+                "${project.version}"
+            })
+            artifact(subProject.tasks["kotlinSourcesJar"])
+            artifact(subProject.tasks["shadowJar"])
+            println("> Apply \"$groupId:$artifactId:$version\"")
+        }
+    }
+}
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-base:1.8.10")
+    }
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+        separateInheritedMembers = false
+        mergeImplicitExpectActualDeclarations = false
+    }
+}
